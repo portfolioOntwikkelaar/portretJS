0d657af6efcb492e3f017b19667973183816118d
noordseoostende <voropayev1@mail.ru>
2022-11-19 18:11:18 +0100

    18:10

--- sketch.js
+++ sketch.js
@@ -1,7 +1,33 @@
-function setup() {
-  createCanvas(400, 400);
+let img;
+let cnv;
+function preload() {
+  img = loadImage('assets/face4.jpeg')
 }
 
-function draw() {
-  background(220);
+function setup() {
+  cnv = createCanvas(img.width, img.height);
+  //print(img.width, img.height)
+  let newCanvasX = (windowWidth - img.width)/2;
+  let newCanvasY = (windowHeight - img.height)/2;
+  cnv.position(newCanvasX, newCanvasY);
+  for (let col = 0; col < img.width; col+=2) {
+    for (let row = 0; row < img.height; row+=2) {
+      let xPos = col;
+      let yPos = row;
+      let c = img.get(xPos,yPos);
+      push();
+
+      translate(xPos, yPos);
+      rotate(radians(random(360)));
+      noFill();
+      stroke(color(c));
+      strokeWeight(random(5));
+      point(xPos, yPos);
+      strokeWeight(random(3));
+
+      //rect(col,row,10,5);
+      curve(xPos, yPos, sin(xPos) * random(60), cos(xPos) * sin(xPos) * random(90), random(10), random(50), cos(yPos) * sin(xPos) * random(120), cos(yPos) * sin(xPos) * 90)
+      pop();
+    }
+  }
 }
